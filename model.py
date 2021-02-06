3163a931e56f0018c360c327225ff21a9dc87626
Katie Aquino <katiemaryfrances.aquino@gmail.com>
2021-02-06 18:56:09 +0000

    Completes Animal and Human class with backrefs

--- model.py
+++ model.py
@@ -11,9 +11,37 @@ db = SQLAlchemy()
 class Human(db.Model):
     """Data model for a human."""
 
+    __tablename__ = "humans"
+
+    human_id = db.Column(db.Integer,
+                        autoincremnt=True,
+                        primary_key=True)
+    fname = db.Column(db.String(25), nullable=False)
+    lname = db.Column(db.String(25), nullable=False)
+    email = db.Column(db.string(100), nullable=False)
+
+    def __repr__(self):
+        return f'<Human: human_id={human_id}, fname={fname}, lname={lname}>'
+
 
 class Animal(db.Model):
     """Data model for an animal."""
+
+    __tablename__ = 'animals'
+
+    animal_id = db.Column(db.Integer,
+                          autoincremnt=True,
+                          primary_key=True)
+    human_id = db.Column(db.Integer, db.ForeignKey('humans.human_id'))
+    name = db.Column(db.String(50), nullable=False)
+    animal_species = db.Column(db.String(25), nullable=False)
+    birth_year = db.Column(db.Integer) #birth year could be unknown, allowing null
+
+    human = db.relationship('Human', backref='animals')
+
+    def __repr__(self):
+        return f"""<Animal: animal_id={animal_id}, name={name}, 
+                    animal_species={animal_species}>"""
 
 
 def connect_to_db(app):
